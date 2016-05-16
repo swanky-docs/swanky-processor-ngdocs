5271ecd86fea5bf1c6925cc560e2efb4e5462ffc
Rod Leviton <rod@rodlevtion.com>
2016-05-16 20:40:30 +1000

    chore(testUnit): add test files and npm script

---
+++ test/spec/unit/index.spec.js
@@ -0,0 +1,51 @@
+const path = require('path');
+// const beforeEach = require('mocha').beforeEach;
+// const afterEach = require('mocha').afterEach;
+// const expect = require('chai').expect;
+// const sinon = require('sinon');
+// const swanky = require(path.resolve('index.js'));
+// const build = require(path.resolve('./tasks/build'));
+// const configUrl = 'test/fixtures/swanky.config.yaml';
+
+describe('Swanky', function () {
+  // var sandbox;
+
+  // beforeEach(function() {
+  //   // create a sandbox
+  //   sandbox = sinon.sandbox.create();
+
+  // });
+
+  // afterEach(function() {
+  //   // restore the environment as it was before
+  //   sandbox.restore();
+  // });
+
+  // describe('loadConfig', function () {
+  //   it('should load the config successfully', function () {
+  //     const config = swanky(configUrl);
+  //     expect(config.loadConfig()).to.be.an('Object');
+  //   });
+
+  //   it('should throw an error when config file does not exist', function () {
+  //     const config = swanky('no-swanky.config.yaml');
+  //     expect(function () { config.loadConfig() }).to.throw(Error);
+  //   });
+  // });
+
+  // describe('generateDocs', function () {
+  //   var buildMock = sinon.stub(build, 'init');
+    
+  //   it('should exist', function () {
+  //     const config = swanky();
+  //     expect(config.generateDocs).to.be.a('Function');
+  //   });
+
+  //   it('should call build', function () {
+  //     const config = swanky(configUrl);
+  //     const configObj = config.loadConfig();
+  //     config.generateDocs();
+  //     sinon.assert.calledWith(buildMock, configObj);
+  //   });
+  // });
+});
