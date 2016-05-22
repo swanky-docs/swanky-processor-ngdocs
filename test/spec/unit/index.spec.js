01b0230fe745a1880135b76e5ce9573931bf3f90
Rod Leviton <rod@rodlevtion.com>
2016-05-22 22:28:57 +1000

    feat(build): add default template

--- test/spec/unit/index.spec.js
+++ test/spec/unit/index.spec.js
@@ -1,51 +1,9 @@
-const path = require('path');
-// const beforeEach = require('mocha').beforeEach;
-// const afterEach = require('mocha').afterEach;
-// const expect = require('chai').expect;
-// const sinon = require('sinon');
-// const swanky = require(path.resolve('index.js'));
-// const build = require(path.resolve('./tasks/build'));
-// const configUrl = 'test/fixtures/swanky.config.yaml';
+var expect = require('chai').expect;
+var swankyNgdocs = require('../../../index');
+var sinon = require('sinon');
 
-describe('Swanky', function () {
-  // var sandbox;
-
-  // beforeEach(function() {
-  //   // create a sandbox
-  //   sandbox = sinon.sandbox.create();
-
-  // });
-
-  // afterEach(function() {
-  //   // restore the environment as it was before
-  //   sandbox.restore();
-  // });
-
-  // describe('loadConfig', function () {
-  //   it('should load the config successfully', function () {
-  //     const config = swanky(configUrl);
-  //     expect(config.loadConfig()).to.be.an('Object');
-  //   });
-
-  //   it('should throw an error when config file does not exist', function () {
-  //     const config = swanky('no-swanky.config.yaml');
-  //     expect(function () { config.loadConfig() }).to.throw(Error);
-  //   });
-  // });
-
-  // describe('generateDocs', function () {
-  //   var buildMock = sinon.stub(build, 'init');
-    
-  //   it('should exist', function () {
-  //     const config = swanky();
-  //     expect(config.generateDocs).to.be.a('Function');
-  //   });
-
-  //   it('should call build', function () {
-  //     const config = swanky(configUrl);
-  //     const configObj = config.loadConfig();
-  //     config.generateDocs();
-  //     sinon.assert.calledWith(buildMock, configObj);
-  //   });
-  // });
+describe('swankyNgdocs', function () {    
+  it('should exist', function () {
+    expect(swankyNgdocs).to.be.a('Function');
+  });
 });
