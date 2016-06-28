86468eed72c7d083615f0518569a2806284f3659
Rod Leviton <rod@rodlevtion.com>
2016-06-28 22:50:46 +1000

    fix(build): add template to fileDeps array

--- index.js
+++ index.js
@@ -24,12 +24,23 @@ var swankyNgdocs = function (page, meta, cb) {
 
       // Read template location from Swanky config
       if (page.processor.ngdocs && page.processor.ngdocs.hasOwnProperty('templates')) {
-        partialTemplate = path.join(basepath, `${page.processor.ngdocs.templates}/${doc.docType}.template.ejs`);
+        partialTemplate = path.join(basepath,
+          `${page.processor.ngdocs.templates}/${doc.docType}.template.ejs`);
       } else {
         partialTemplate = path.join(__dirname, `./templates/api/${doc.docType}.template.ejs`);
       }
 
-      var str = fs.readFileSync(partialTemplate, 'utf-8');
+      var str;
+
+      try {
+        str = fs.readFileSync(partialTemplate, 'utf-8');
+      } catch (err) {
+        console.log(err);
+        process.exit(1);
+      }
+
+      // Add file deps for live reload / webpack implementation
+      page.fileDependencies.push(partialTemplate);
 
       var options = {
         filename: partialTemplate,
